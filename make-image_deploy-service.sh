b88a3afe400bbe2b45882c4a2041ac35b698d791
kingdo777 <1440852110@qq.com>
2019-09-05 21:30:49 +0800

    doc原装+制作镜像+部署服务

---
+++ make-image_deploy-service.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo docker build -t kingdo/knative-helloworld-python .
+sudo docker push kingdo/knative-helloworld-python
+kubectl apply --filename service.yaml
+kubectl get pods
