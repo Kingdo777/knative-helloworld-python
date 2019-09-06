0ab1a2f2001b700b9b2ed686e0dd6b09c6b2fa37
kingdo777 <1440852110@qq.com>
2019-09-06 21:26:54 +0800

    修改yaml中HPA的参数

--- make-image_deploy-service.sh
+++ make-image_deploy-service.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo docker build -t kingdo/knative-helloworld-python .
 sudo docker push kingdo/knative-helloworld-python
+kubectl delete --filename service.yaml
 kubectl apply --filename service.yaml
 kubectl get pods
