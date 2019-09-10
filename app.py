ac034ac29b57d91d32ffdae3a9180f182d12214c
kingdo777 <1440852110@qq.com>
2019-09-10 20:01:16 +0800

    再次减小cpu使用量

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    for i in range(10000000):
+    for i in range(1000000):
         i += 1
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
